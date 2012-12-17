7bddd57f26b1e0e7159fcd8af5bbde30358148d0
Cynthia Kiser <cnk@caltech.edu>
2012-12-16 17:11:21 -0800

    before refactoring for formatters

--- lib/todo/todo.rb
+++ lib/todo/todo.rb
@@ -1,14 +1,80 @@
 module Todo
   def new_task(filename, task_names)
-    File.open(filename, 'a+') do |todo_file|
-      tasks = 0
-      task_names.each do |task|
-        todo_file.puts [task, Time.now].join(',')
-        tasks += 1
+    begin
+      File.open(filename, 'a+') do |todo_file|
+        tasks = 0
+        task_names.each do |task|
+          todo_file.puts [task, Time.now].join(',')
+          tasks += 1
+        end
+        if tasks == 0
+          raise "You must provide tasks on the command-line or from standard input"
+        end
       end
-      if tasks == 0
-        raise "You must provide tasks on the command-line or from standard input"
-      end
+    rescue SystemCallError => err
+      raise RuntimeError, "Couldn't open #{filename} for appending: #{err.message}"
     end
   end
+
+  def list_tasks(filename, format)
+    begin
+      File.open(filename, 'r') do |file|
+        counter = 1
+        file.readlines.each do |line|
+          format == 'pretty'? display_todo(line, counter) : machine_readable_todo(line, counter)
+          counter += 1
+        end
+      end
+    rescue SystemCallError => err
+      raise RuntimeError, "Couldn't open #{filename} for reading: #{err.message}"
+    end
+  end
+
+  def complete_task(infile, outfile, completed_item)
+    begin
+    File.open(infile, 'r') do |infile|
+      File.open(outfile, 'w') do |outfile|
+        counter = 1
+        infile.readlines.each do |line|
+          if completed_item == counter
+            # Write out existing data plus completion time
+            name, created, completed = read_todo(line)
+            write_todo(outfile, name, created, Time.now)
+          else
+            # Just copy the existing data to the new file
+            write_todo(outfile, *read_todo(line))
+          end
+          counter += 1
+        end
+      end
+    end
+    rescue SystemCallError => err
+      raise RuntimeError, "Couldn't open #{filename} for reading: #{err.message}"
+    end
+
+  end
+    
+
+
+  ########## helper methods for list_tasks #########
+
+  def display_todo(line, counter)
+    name, created, completed = read_todo(line)
+    printf("%3d - %s\n", counter,name) 
+    printf("      Created:   #{created} \n")
+    unless completed.nil?
+      printf ("      Completed: #{completed}\n")
+    end
+  end
+  
+  def machine_readable_todo(line, counter)
+    name, created, completed = read_todo(line)
+    completed_flag = completed ? "C" : "U"
+    printf("%d,%s,%s,%s,%s\n",counter,name,completed_flag,created,completed)
+  end
+  
+  def read_todo(line)
+    line.chomp.split(',')
+  end
+
 end
--- test/todo_test.rb
+++ test/todo_test.rb
@@ -10,17 +10,53 @@ require 'mocha/setup'
 require 'stringio'
 
 describe "Todo actions" do
-  it "should raise an error when there are no tasks" do
-    err = lambda {Todo.new_task('todo.txt', [])}.must_raise RuntimeError
-    err.message.must_match "You must provide tasks on the command-line or from standard input"
+  describe "list" do
+    before do
+      @fake_file = StringIO.new("Completed task,2012-12-12 18:10:00 -0800,2012-12-12 22:50:00 -0800\nUncompleted task,2012-12-12 18:12:00 -0800,")
+      File.stubs(:open).yields(@fake_file)
+    end
+
+    it "should generate machine-readable output if asked" do
+      output = list_tasks("file.txt", 'csv')
+      output.must_equal "1,Completed task,C,2012-12-12 18:10:00 -0800,2012-12-12 22:50:00 -0800\n2,Uncompleted task,U,2012-12-12 18:12:00 -0800,"
+    end
+
+    def teardown 
+      File.unstub(:open)
+    end
   end
 
-  it "should add tasks to the task list" do
-    fake_file = StringIO.new
-    File.stubs(:open).yields(fake_file)
-    new_task('todo-list.txt', ["Test task"])
+  describe "done" do
+    # "This is a bigger job to test because of the 2 files."
+  end
+
+  describe "new" do
+    it " should raise an error when there are no tasks" do
+      File.stubs(:open).yields(StringIO.new)
+      err = lambda {Todo.new_task('problems.txt', [])}.must_raise RuntimeError
+      err.message.must_match "You must provide tasks on the command-line or from standard input"
+    end
+    
+    it "should add tasks to the task list" do
+      fake_file = StringIO.new
+      File.stubs(:open).yields(fake_file)
+      new_task('todo-list.txt', ["Test task"])
     assert_match /^Test task,/, fake_file.string
-  end
+    end
 
+    it "should tell you if it can't open the file" do
+      ex_msg = "Operation not permitted"
+      File.stubs(:open).raises(Errno::EPERM.new(ex_msg))
+      ex = assert_raises RuntimeError do
+        new_task("foo.txt",["This is a task"])
+      end
+      assert_match /^Couldn't open foo.txt for appending: #{ex_msg}/, ex.message
+    end
+
+    def teardown 
+      File.unstub(:open)
+    end
+  end
+  
 end
 
